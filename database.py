42e66c2d733f06bdc0770ca75b1af96cb6a5c23d
AnujaBhise <anujabhise23201@gmail.com>
2026-05-29 15:46:22 +0530

    removed test.py

--- database.py
+++ database.py
@@ -1,28 +1,4 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import declarative_base, sessionmaker
 
-# # SQL Server Connection
-# SQLALCHEMY_DATABASE_URL = (
-#     "mssql+pyodbc://@DESKTOP-3QGTICG\\MSSQLSERVER01/ExpenseTracker?"
-#     "driver=ODBC+Driver+17+for+SQL+Server&"
-#     "trusted_connection=yes"
-# )
-
-# # Create Engine
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL,
-#     echo=True
-# )
-
-# # Session
-# SessionLocal = sessionmaker(
-#     autocommit=False,
-#     autoflush=False,
-#     bind=engine
-# )
-
-# # Base
-# Base = declarative_base()
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
@@ -52,10 +28,3 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 
-# Dependency for FastAPI
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
